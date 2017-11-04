3e88625570c791276952915233a5337e8bc01c06
Tony <antn89@gmail.com>
2017-11-04 17:12:42 +0100

    Wrote solution for 'Sorting: Bubble Sort' HackerRank problem (Cracking the Coding Interview Section)

--- src/hackerranksolutions/Solution.java
+++ src/hackerranksolutions/Solution.java
@@ -5,6 +5,8 @@
  */
 package hackerranksolutions;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tony
@@ -15,7 +17,65 @@ public class Solution {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+       Scanner in = new Scanner(System.in);
+        int numberOfElements = in.nextInt();
     
+        int[] a = new int[numberOfElements]; 
+    
+        for (int i = 0; i < numberOfElements; i ++){
+            
+            a[i] = in.nextInt();
+           
+            
+        }
+        
+        
+        boolean isSorted = false;
+        Integer lenghtToSort = a.length -1;
+        Integer  numberOfSwaps = 0; 
+        
+        
+        while(!isSorted){
+            
+            isSorted = true;
+            
+            for (int i = 0; i < lenghtToSort; i++){
+                
+                if(a[i] > a[i+1]){
+                    
+                    swap(a,i,i+1);
+                    numberOfSwaps = numberOfSwaps + 1;
+                    isSorted = false;
+                    
+                }
+                 
+            }
+            lenghtToSort--;
+            
+            
+            
+            
+        }
+        
+          System.out.println("Array is sorted in "+ numberOfSwaps + " swaps.");
+          System.out.println("First Element: "+ a[0]); 
+          System.out.println("Last Element: "+ a[numberOfElements-1]);
+
+        
+        
+        
+       
+            
+    
+    }
+
+    private static void swap(int[] a, int i, int i0) {
+
+        a[i] = a[i]+ a[i0];
+        a[i0] = a[i] - a[i0];
+        a[i] = a[i] - a[i0];
+        
+
+
+    }
 }
